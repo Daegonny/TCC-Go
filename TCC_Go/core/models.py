05e9e6cbc569870a4d0dd8f973f10160e479257a
Your Name <you@example.com>
2017-09-20 10:29:17 -0300

    Adicionado o User padrão

--- TCC_Go/core/models.py
+++ TCC_Go/core/models.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    registration = models.CharField(max_length=30, null=False, blank=False, unique=True)
+    course = models.CharField(max_length=30, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
